6048532cf2fba7a5abc54481e7098b74ac9eccea
alinaguselnikova <a.guselnikova@g.nsu.ru>
2021-10-03 14:27:54 +0700

    lab1 Fixed

--- Task_1_1_1/src/main/java/ru/nsu/fit/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/fit/HeapSort.java
@@ -1,47 +1,52 @@
-package ru.nsu.fit;
+    package ru.nsu.fit;
 
-//Создаём единственный публичный класс Heapsort
-public class HeapSort {
-    int[] heap;
-    //Создаём конструктор Heapsort
-    public HeapSort(int n) {
-        this.heap = new int[n];
-    }
-    //Создаём публичный метод sort, не возвращающий ничего и работающий с массивом
-     public void sort(int[] arr) {
-        int n = arr.length;
-        //Строим кучу из исходного массива(max heap)
-        for (int i = n/2 - 1; i >= 0; i--)
-            heap(arr, n, i);
-        //Создаём min heap
-        for (int i = n - 1; i >= 0; i--) {
-            int c = arr[0];
-            arr[0] = arr[i];
-            arr[i] = c;
-            heap(arr, i, 0);
+
+    public  class HeapSort {
+
+        /**  Сортировка массива
+         *
+         * @param arr входной массив
+         */
+         public static void sort(int[] arr) {
+             if (arr == null){
+                 throw new IllegalArgumentException("Нулевой указатель");
+             }
+            int n = arr.length;
+            for (int i = n/2 - 1; i >= 0; i--)
+                heap(arr, n, i);
+            for (int i = n - 1; i >= 0; i--) {
+                int c = arr[0];
+                arr[0] = arr[i];
+                arr[i] = c;
+                heap(arr, i, 0);
+            }
+        }
+
+        /** Пирамидальная сортировка
+         * @param arr входной массив
+         * @param n размер массива
+         * @param i номер элемента, от которого происходит сортировка
+         */
+        private static void heap(int[] arr, int n, int i) {
+            int root = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (l < n && arr[l] > arr[root])
+                root = l;
+
+            if (r < n && arr[r] > arr[root])
+                root = r;
+
+            if (root != i) {
+                int swap = arr[i];
+                arr[i] = arr[root];
+                arr[root] = swap;
+
+                heap(arr, n, root);
+            }
         }
     }
-    //Пирамидальная сортировка
-    void heap(int[] arr, int n, int i) {
-        int root = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-
-        if (l < n && arr[l] > arr[root])
-            root = l;
-
-        if (r < n && arr[r] > arr[root])
-            root = r;
-
-        if (root != i) {
-            int swap = arr[i];
-            arr[i] = arr[root];
-            arr[root] = swap;
-
-            heap(arr, n, root);
-        }
-    }
-}
 
 
 
