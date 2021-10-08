41d556c09eeacc190bfb76cff2b9608dcacd9763
alinaguselnikova <a.guselnikova@g.nsu.ru>
2021-10-08 15:12:07 +0700

    Tests fixed

--- Task_1_1_1/src/test/java/nsu/fit/Tests.java
+++ Task_1_1_1/src/test/java/nsu/fit/Tests.java
@@ -15,11 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Tests {
-    static public ArrayList <int[]> arrays() {
+    static public ArrayList<int[]> arrays() {
         var list = new ArrayList<int[]>();
 
         list.add(new int[]{6, 3, 0, 2, 7});
-        list.add(new int[]{-198,-100, -6, -2, -1});
+        list.add(new int[]{-198, -100, -6, -2, -1});
         list.add(new int[]{-100, 100, 256, -887, 0});
         list.add(new int[]{-256, 0, 3, 96, 500});
         list.add(new int[]{55, -2, 97, 55, -88});
@@ -35,20 +35,16 @@ public class Tests {
     @MethodSource("arrays")
     void comp(int[] array) {
         int[] res = array.clone();
-
         sort(array);
         Arrays.sort(res, 0, res.length);
+        assertArrayEquals(array, res);
+    }
 
-        assertArrayEquals(array,res);
-
-        }
     @ParameterizedTest
     @NullSource
-    void TestNull(int[] input){
+    void TestNull(int[] input) {
         assertThrows(IllegalArgumentException.class, () -> HeapSort.sort(input));
     }
-
-
-    }
+}
 
 
