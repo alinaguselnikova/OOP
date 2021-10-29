e1af595de17b996873305f5d929b04956af21126
alinaguselnikova <a.guselnikova@g.nsu.ru>
2021-10-29 13:17:19 +0700

    Stack with Iterable Interface

---
+++ Task 2_1_1/src/main/java/nsu/computerScience/stackImplementation/Stack.java
@@ -0,0 +1,92 @@
+package nsu.computerScience.stackImplementation;
+
+
+import java.util.*;
+
+
+public class Stack<T> implements Iterable<T> {
+    public T[] stackArray = (T[]) new Object[10];
+    int size = 10;
+    int last = 0;
+
+    public void push(T element) {
+        if (this.last >= stackArray.length) {
+            T[] stackPush = (T[]) new Object[size * 2];
+            size *= 2;
+            stackPush = Arrays.copyOf(stackArray, size);
+            stackArray = stackPush;
+        }
+        stackArray[last++] = element;
+    }
+
+    public T pop() {
+        if (size <= 0) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            last--;
+            T element = stackArray[last];
+            stackArray[last] = null;
+            return element;
+        }
+    }
+
+    public void pushStack(Stack<T> pushEx) {
+        if (pushEx == null) {
+            throw new IllegalArgumentException("Trying to push null stack.");
+        }
+
+        int len = pushEx.last;
+        if (len <= 0) {
+            return;
+        }
+        if (size + len >= stackArray.length) {
+            size += pushEx.last + 1;
+            T[] stackPush = (T[]) new Object[size];
+            stackPush = Arrays.copyOf(stackArray, size);
+            stackArray = stackPush;
+        }
+        System.arraycopy(pushEx.stackArray, 0, this.stackArray, last, len);
+        last += len;
+
+
+    }
+
+    public Stack<T> popStack(int n) {
+        if (n > last)
+            throw new IndexOutOfBoundsException();
+        Stack<T> fin = new Stack<>();
+        T[] add = (T[]) new Object[this.last + 1];
+        for (int i = 0; i < n; i++) {
+            add[i] = this.pop();
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            fin.push(add[i]);
+        }
+        return fin;
+    }
+
+    public boolean isEmpty() {
+        return last == 0;
+    }
+
+    public int count() {
+        return stackArray.length;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new StackIterator();
+    }
+    class StackIterator implements Iterator<T>{
+        private int position =  0;
+        @Override
+        public boolean hasNext() {
+            return !(position == last);
+        }
+
+        @Override
+        public T next() throws NoSuchElementException {
+            return stackArray[position++] ;
+        }
+    }
+}
---
+++ Task 2_1_1/src/test/java/nsu/computerScience/stackImplementation/StackTest.java
@@ -0,0 +1,98 @@
+package nsu.computerScience.stackImplementation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class StackTest {
+
+   /* public static void main(String[] args) {
+        Stack<Integer> exampleSt = new Stack<>();
+        exampleSt.push(2);
+        exampleSt.push(7);
+        Stack<Integer> anotherSt = new Stack<>();
+        anotherSt.push(4);
+        anotherSt.push(8);
+        exampleSt.pushStack(anotherSt);
+        exampleSt.pop();
+        exampleSt.popStack(2);
+
+    }
+    */
+
+    @Test
+    public void testExample() {
+        Stack<Integer> exampleStack = new Stack<>();
+        exampleStack.push(2);
+        exampleStack.push(7);
+        Stack<Integer> anotherStack = new Stack<>();
+        anotherStack.push(4);
+        anotherStack.push(8);
+        exampleStack.pushStack(anotherStack);
+        exampleStack.pop();
+        exampleStack.popStack(2);
+        Assertions.assertEquals(2, exampleStack.pop());
+        assertThrows(IndexOutOfBoundsException.class, exampleStack::pop);
+    }
+
+    Stack<Integer> tStack = new Stack<>();
+    ArrayList<Integer> arr;
+
+    {
+        arr = new ArrayList<>();
+    }
+
+
+    @Test
+    public void testPopPush() {
+        tStack.push(5);
+        tStack.push(4);
+        tStack.push(3);
+        Assertions.assertEquals(3, tStack.pop());
+        Assertions.assertEquals(4, tStack.pop());
+        Assertions.assertEquals(5, tStack.pop());
+    }
+
+    @Test
+    public void testPushStack() {
+        Stack<Integer> exStack = new Stack<>();
+        exStack.push(5);
+        exStack.push(4);
+        exStack.push(3);
+        tStack.pushStack(exStack);
+        Assertions.assertEquals(3, tStack.pop());
+        Assertions.assertEquals(4, tStack.pop());
+        Assertions.assertEquals(5, tStack.pop());
+    }
+
+    @Test
+    public void testPopStack() {
+        tStack.push(5);
+        tStack.push(4);
+        tStack.push(3);
+        Stack<Integer> PopStack = tStack.popStack(2);
+        Assertions.assertEquals(3, PopStack.pop());
+        Assertions.assertEquals(4, PopStack.pop());
+        assertThrows(IndexOutOfBoundsException.class, PopStack::pop);
+    }
+
+    @Test
+    public void testIterator() {
+        ArrayList <Integer> arr = new ArrayList<>();
+        for (var i = 0; i < 10; i++) {
+            tStack.push(i);
+            arr.add(i);
+        }
+        Iterator<Integer> iter = tStack.iterator();
+        Iterator<Integer> iterArr = arr.iterator();
+        for (var elem : tStack) {
+        Assertions.assertEquals(iter.next(), iterArr.next());
+        }
+    }
+
+}
